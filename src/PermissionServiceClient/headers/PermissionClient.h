a176b21b6888a245f23e0d2907282e2eac79c223
Lightning <sergejananev78@gmail.com>
2024-10-30 03:36:54 +0300

    Permission Service Client

---
+++ src/PermissionServiceClient/headers/PermissionClient.h
@@ -0,0 +1,22 @@
+#ifndef PERMISSIONCLIENT_H
+#define PERMISSIONCLIENT_H
+
+#include <sdbus-c++/sdbus-c++.h>
+
+#include "PermissionServiceClient.h"
+
+class PermissionClient
+    : public sdbus::ProxyInterfaces<com::system::permissions_proxy>
+{
+   public:
+    PermissionClient(sdbus::ServiceName destination,
+                     sdbus::ObjectPath objectPath)
+        : ProxyInterfaces(std::move(destination), std::move(objectPath))
+    {
+        registerProxy();
+    }
+
+    ~PermissionClient() { unregisterProxy(); }
+};
+
+#endif  // PERMISSIONCLIENT_H
