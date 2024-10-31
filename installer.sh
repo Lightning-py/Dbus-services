37c429f6c035b6c2ada0fd8c1305da8fd4e51c8f
Sergey <sergejananev78@gmail.com>
2024-10-31 03:12:11 +0300

    Fixed installer

--- installer.sh
+++ installer.sh
@@ -1,16 +1,15 @@
+sudo apt install git build-essential cmake libsystemd-dev sqlite3 libsqlite3-dev
+
 git clone https://github.com/Kistler-Group/sdbus-cpp
 cd sdbus-cpp
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release 
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSDBUSCPP_BUILD_LIBSYSTEMD=OFF 
 cmake --build .
 sudo cmake --build . --target install
 
 cd ../..
-
-sudo apt update
-sudo apt install sqlite3 libsqlite3-dev
 
 mkdir build
 cd build
